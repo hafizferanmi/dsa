c83218117cad6f0a5791fdcbceb7656a9ff9aa5a
Hafizferanmi <hafizferanmi@gmail.com>
2021-07-08 12:33:34 +0100

    Array operations

---
+++ array.js
@@ -0,0 +1,20 @@
+const arrays = ["hafiz", "ferranmi", "is", "a", "good", "boy"];
+
+// Operations on arrays
+
+// PUSH - o(1)
+// POP - o(1)
+// ACCESS - o(1)
+// INSERT - 0(n)
+
+arrays.push("value");
+console.log(arrays);
+
+arrays.pop();
+console.log(arrays);
+
+arrays.shift();
+console.log(arrays);
+
+arrays.unshift("Boda");
+console.log(arrays);
